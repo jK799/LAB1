fee6e100723e03896de5b8f85646383b82eff193
jK799 <j.hruby79@gmail.com>
2024-10-27 09:28:00 +0100

    init

---
+++ Shape.java
@@ -0,0 +1,12 @@
+public class Shape {
+
+    public void print(){
+        System.out.println("Shape");
+    }
+
+    public static void main(String[] args){
+        Shape shape = new Shape();
+        shape.print();
+    }
+
+}
